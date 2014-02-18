0ea085b8192496856d820e5fed1d78d48f94e8d3
oozlegit <qwapoozle@gmail.com>
2014-02-18 17:46:17 -0500

    admin checking system

--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
-for i=1,10 do
-	print("hello world")
+--script accepts both usernames and userids (in case you want to really make sure someone stays admin by checking their userid)
+local adminnames = {"PlusReed", "Oozlebachr", "Enfys"}
+local adminids = {13135356, 4353611, 36305601}
+
+function checkifadmin(player)
+	--check usernames
+	for i,v in ipairs(admins) do
+		if string.lower(v) == string.lower(player.Name) then
+			return true
+		end
+	end
+	--check userids
+	for i,v in ipairs(adminids) do
+		if v == player.userId then
+			return true
+		end
+	end
 end
+	
+
+game:GetService("Players").PlayerAdded:connect(function(player)
+	if checkifadmin(player) then
+		--insert admin things here
+	end
+end)
