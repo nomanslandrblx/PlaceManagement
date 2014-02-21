cdac9311ea555f546cd388e90ee8573b99579ee5
oozlegit <qwapoozle@gmail.com>
2014-02-21 15:52:07 -0500

    modulescript things

--- config.lua
+++ config.lua
@@ -5,7 +5,9 @@
 
 -- local adminnames = {"PlusReed", "Oozlebachr", "Enfys", "CoffeeScripter"}
 local adminids = {13135356, 4353611, 36305601, 6110966}
-local bannednames = {"NowDoTheHarlemShake"}
+--local bannednames = {"NowDoTheHarlemShake"}
 local bannedids = {38506985}
 
 local autokick = true -- can be used to write custom ban handlers [CS]
+
+return {adminids,bannedids,autokick}
