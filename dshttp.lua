194bed664808bcda6b8226cd5600e9c7d4417504
Sean <coffeescripter@gmail.com>
2014-02-22 22:25:59 +0000

    rewrote DSHTTP

--- dshttp.lua
+++ dshttp.lua
@@ -1,26 +1,17 @@
---[[ DATASTORE AND HTTPSERVICE ]]--
---[[ ENSURE IT'S MODULESCRIPT ]]--
-
-local dshttp = {}
-
---[[ DATASTORE ]]--
+--DataStore
 local globaldatastore = game:GetService('DataStoreService'):GetGlobalDataStore()
--- `Xmodule` is used instead of `module` as `module` is a Lua keyword
-function dshttp.WritePPMCoreDS(Xmodule, key, value)
-	globaldatastore:SetAsync('PPM_' .. Xmodule .. '_' .. key, value)
+
+function PPM.DataStore.Write(module_, key, value)
+	globaldatastore:WriteAsync('PPM_' .. module_ .. '_' .. key, value)
 end
 
-function dshttp.GetPPMCoreDS(Xmodule, key)
-	return globaldatastore:GetAsync('PPM_' .. Xmodule .. '_' .. key)
-end
-	
---[[ HTTPSERVICE ]]--
-local httpservice = game:GetService('HttpService')
-
-function dshttp.GetHttpService()
-	return httpservice
+function PPM.DataStore.Write(module_, key)
+	globaldatastore:GetAsync('PPM_' .. module_ .. '_' .. key)
 end
 
+-- HttpService
+local httpserv = game:GetService('HttpService')
 
---[[ READY ]]--
-return dshttp
+function PPM.HttpServ.GetServ()
+	return httpserv
+end
