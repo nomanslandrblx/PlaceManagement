07f94223f66e7596273e183bbedd954d2dfd11c4
oozlegit <qwapoozle@gmail.com>
2014-02-22 09:59:32 -0500

    formatting 4 modulescript things

--- dshttp.lua
+++ dshttp.lua
@@ -1,20 +1,26 @@
 --[[ DATASTORE AND HTTPSERVICE ]]--
 --[[ ENSURE IT'S MODULESCRIPT ]]--
 
+local dshttp = {}
+
 --[[ DATASTORE ]]--
 local globaldatastore = game:GetService('DataStoreService'):GetGlobalDataStore()
 -- `Xmodule` is used instead of `module` as `module` is a Lua keyword
-function WritePPMCoreDS(Xmodule, key, value)
+function dshttp.WritePPMCoreDS(Xmodule, key, value)
 	globaldatastore:SetAsync('PPM_' .. Xmodule .. '_' .. key, value)
 end
 
-function GetPPMCoreDS(Xmodule, key)
+function dshttp.GetPPMCoreDS(Xmodule, key)
 	return globaldatastore:GetAsync('PPM_' .. Xmodule .. '_' .. key)
 end
 	
 --[[ HTTPSERVICE ]]--
 local httpservice = game:GetService('HttpService')
 
-function GetHttpService()
+function dshttp.GetHttpService()
 	return httpservice
 end
+
+
+--[[ READY ]]--
+return dshttp
