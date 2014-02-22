461bc9295552f5cef39108dd603c9313b59a8798
oozlegit <qwapoozle@gmail.com>
2014-02-21 23:42:38 -0500

    added documentation for printmodule

--- modules/printmodule.lua
+++ modules/printmodule.lua
@@ -4,5 +4,10 @@ return {
 	"doprint",
 	function(arg1,arg2)
 		print(arg1,arg2)
-	end
+	end,
+	[[==========doprint by oozlebachr
+		
+doprint is an example module used for reference or testing.
+		
+doprint has two arguments and will print both of them.]]
 }
